72dd7fbe5cba784ac25fe89d9f4bdf034aebb397
Seppe Mestdagh <mestdaghseppe@gmail.com>
2024-03-09 16:41:51 +0100

    Implemented ImGui

--- Minigin/InputManager.cpp
+++ Minigin/InputManager.cpp
@@ -1,8 +1,12 @@
 #include <SDL.h>
 #include "InputManager.h"
+#include <backends/imgui_impl_sdl2.h>
 
 bool dae::InputManager::ProcessInput()
 {
+	//ZeroMemory(&m_CurrentState, sizeof(XINPUT_STATE));
+	//XInputGetState(0, &m_CurrentState);
+
 	SDL_Event e;
 	while (SDL_PollEvent(&e)) {
 		if (e.type == SDL_QUIT) {
@@ -14,7 +18,8 @@ bool dae::InputManager::ProcessInput()
 		if (e.type == SDL_MOUSEBUTTONDOWN) {
 			
 		}
-		// etc...
+
+		ImGui_ImplSDL2_ProcessEvent(&e);
 	}
 
 	return true;
--- Minigin/InputManager.h
+++ Minigin/InputManager.h
@@ -1,12 +1,15 @@
 #pragma once
 #include "Singleton.h"
 
+
 namespace dae
 {
 	class InputManager final : public Singleton<InputManager>
 	{
 	public:
 		bool ProcessInput();
+	//private:
+	//	XINPUT_STATE m_CurrentState;
 	};
 
 }
